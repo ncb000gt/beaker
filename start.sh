1abd5157425752a07e277d6bf0248a16856892c3
Nick Campbell <nicholas.j.campbell@gmail.com>
2009-09-02 21:38:00 -0400

    Build script, specific to ubuntu using apt-get. Also, start script now runs server with package naming.

--- start.sh
+++ start.sh
@@ -5,5 +5,4 @@ SCALA=`which scala`
 L=libs
 cp=`echo $L/*.jar|sed 's/ /:/g'`
 
-echo "$SCALA -classpath $cp server"
-$SCALA -cp "$cp:./build/" server
+$SCALA -cp "$cp:./build/" com.digitaltumbleweed.server.server
